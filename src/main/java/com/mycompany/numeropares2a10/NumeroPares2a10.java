661d5924ace4befc200807f1a1c2b2f082570c1e
jorgeJimz <127570326+jorgeJimz@users.noreply.github.com>
2023-03-10 20:27:55 -0600

    Add files via upload

---
+++ src/main/java/com/mycompany/numeropares2a10/NumeroPares2a10.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.numeropares2a10;
+
+/**
+ *
+ * @author Usuario
+ */
+public class NumeroPares2a10 {
+
+    public static void main(String[] args) {
+        int num = 1;
+        
+        for(num = 1; num <=100; num++){
+            if(num%2 == 0)
+                System.out.println(num + " Numero Par");
+       
+      }
+            
+    }
+}
